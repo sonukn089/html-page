2e4fc238f382aa63b9e5261d1ac00b9d34508188
Sonu <sonukn089@gmail.com>
2018-05-12 00:49:13 +0530

    create index.php

--- index.php
+++ index.php
@@ -5,7 +5,13 @@
 </head>
 <body>
 
-	<h1>First Heading</h1>
+	<h1>My First Heading</h1>
+	<p>My First paragraph.</p>
+	<hr>
+	<h1>This is heading 1</h1>
+	<h2>This is heading 2</h2>
+	<h3>This is heading 1</h3>
+	<h4>This is heading 1</h4>
 
 </body>
 </html>
