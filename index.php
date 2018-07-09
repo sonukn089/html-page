cdc6497db195014e6726316bd9a20450f42e2b3d
Sonu <sonukn089@gmail.com>
2018-07-09 23:49:47 +0530

    add index.php

--- index.php
+++ index.php
@@ -1,37 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Page Title</title>
+	<title>My Page</title>
 </head>
 <body>
-
-	<h1>My First Heading</h1>
-	<p>My First paragraph.</p>
-	<hr>
-	<h1>This is heading 1</h1>
-	<h2>This is heading 2</h2>
-	<h3>This is heading 1</h3>
-	<h4>This is heading 1</h4>
-	<hr>
-	<p>This is a paragraph.</p>
-	<p>This is another paragraph.</p>
-	<hr>
-	<a href="https://www.w3schools.com">W3 School</a><br>
-	<a href="https://www.google.com">Google Page</a><br>
-	<img src="sonu.jpg" height="200" width="300"><br>
-	<button>Click Me</button><br>
-	<h2>An Unordered HTML List</h2>
-	<ul>
-		<li>Coffee</li>
-		<li>Tea</li>
-		<li>Milk</li>
-	</ul>
-	<h2>An Ordered HTML List</h2>
-	<ol>
-		<li>Coffee</li>
-		<li>Tea</li>
-		<li>Milk</li>
-	</ol>
 
 </body>
 </html>
