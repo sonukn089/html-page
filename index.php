9b46358a04ec9d532790ab39ca3c7b1125593694
Sonu <sonukn089@gmail.com>
2018-07-11 00:25:55 +0530

    add heading and paragraph

--- index.php
+++ index.php
@@ -10,7 +10,12 @@
 	<h3>It's my Third heading...</h3>
 	<h4>It's my Fourth heading...</h4>
 
-	
+	<p>Sample text is being <strong>cut and pasted again</strong> and again. This is a sample Blog post.  Sample text is being cut and pasted again and again. This is a sample Blog post.  Sample text is being cut and pasted again and again. This is a sample Blog post.  Sample text is being cut and pasted again and again. This is a sample Blog post.  Sample text is being cut and pasted again and again.</p>
+
+	<p>Sample text is being cut and pasted again and again. This is a sample Blog post.  Sample text is being cut and <mark>pasted again</mark> and again. This is a sample Blog post.  Sample text is being cut and pasted again and again.</p>
+
+	<p>sample Blog post.  Sample text is being cut and pasted again and again. This is a sample Blog post. <mark><i>Sample text is being cut and pasted again</i></mark> and again. This is a sample Blog post. Sample text is being cut and pasted again and again. This is a sample Blog post.  Sample text is being cut and pasted again and again.</p>
+
 
 </body>
 </html>
